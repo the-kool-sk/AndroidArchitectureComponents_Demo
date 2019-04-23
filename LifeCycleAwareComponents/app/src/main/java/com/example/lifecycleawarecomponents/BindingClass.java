638a5d4c2d568f742037ee825017e4eea224a242
the-kool-sk <surajprasannakulkarni@gmail.com>
2019-04-23 22:08:59 +0530

    Minor Changes

---
+++ LifeCycleAwareComponents/app/src/main/java/com/example/lifecycleawarecomponents/BindingClass.java
@@ -0,0 +1,54 @@
+package com.example.lifecycleawarecomponents;
+
+import android.content.Context;
+import android.location.LocationListener;
+import android.util.Log;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.OnLifecycleEvent;
+
+public class BindingClass implements LifecycleObserver {
+
+    BindingClass(LifecycleOwner lifecycleOwner) {
+        lifecycleOwner.getLifecycle().addObserver(this);
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    void oncreateIsCalled()
+    {
+        Log.i("State","OnCreatedState");
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    void onStartIsCalled()
+    {
+        Log.i("State","OnStartedState");
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    void onresumeIsCalled()
+    {
+        Log.i("State","OnResumedState");
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    void onpauseIsCalled()
+    {
+        Log.i("State","OnPausedState");
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    void onstopIsCalled()
+    {
+        Log.i("State","OnStoppedState");
+    }
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    void ondestroyIsCalled()
+    {
+        Log.i("State","OnDestroyedState");
+    }
+
+}
---
+++ LifeCycleAwareComponents/app/src/main/java/com/example/lifecycleawarecomponents/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.lifecycleawarecomponents;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LifecycleOwner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new BindingClass(this);
+
+    }
+}
