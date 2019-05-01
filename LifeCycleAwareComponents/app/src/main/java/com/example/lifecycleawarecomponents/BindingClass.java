26ab04a612b4d9f7cd704f1315e5fb342c1dc407
the-kool-sk <surajprasannakulkarni@gmail.com>
2019-05-01 12:44:38 +0530

    changes

--- LifeCycleAwareComponents/app/src/main/java/com/example/lifecycleawarecomponents/BindingClass.java
+++ LifeCycleAwareComponents/app/src/main/java/com/example/lifecycleawarecomponents/BindingClass.java
@@ -12,46 +12,61 @@ import androidx.lifecycle.OnLifecycleEvent;
 
 public class BindingClass implements LifecycleObserver {
 
+    LifecycleOwner lifecycleOwner;
      BindingClass(LifecycleOwner lifecycleOwner) {
 
+            this.lifecycleOwner=lifecycleOwner;
             lifecycleOwner.getLifecycle().addObserver(this);
+            Log.i("ActivityLifeCycleStatus", lifecycleOwner.getLifecycle().getCurrentState().toString()+" State");
+
         }
 
         @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
         void oncreateIsCalled ()
         {
-
-            Log.i("State", "OnCreatedState");
+            Log.i("ActivityLifeCycleStatus", "OnCreated-ObserverEvent");
+            Log.i("ActivityLifeCycleStatus", lifecycleOwner.getLifecycle().getCurrentState().toString()+" State");
         }
 
         @OnLifecycleEvent(Lifecycle.Event.ON_START)
         void onStartIsCalled ()
         {
-            Log.i("State", "OnStartedState");
+            Log.i("ActivityLifeCycleStatus", "OnStarted-ObserverEvent");
+            Log.i("ActivityLifeCycleStatus", lifecycleOwner.getLifecycle().getCurrentState().toString()+" State");
         }
 
         @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
         void onresumeIsCalled ()
         {
-            Log.i("State", "OnResumedState");
+            Log.i("ActivityLifeCycleStatus", "OnResumed-ObserverEvent");
+            Log.i("ActivityLifeCycleStatus", lifecycleOwner.getLifecycle().getCurrentState().toString()+" State");
 
         }
 
         @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
         void onpauseIsCalled ()
         {
-            Log.i("State", "OnPausedState");
+            Log.i("ActivityLifeCycleStatus", "OnPaused-ObserverEvent");
+            Log.i("ActivityLifeCycleStatus", lifecycleOwner.getLifecycle().getCurrentState().toString()+" State");
         }
 
         @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
         void onstopIsCalled ()
         {
-            Log.i("State", "OnStoppedState");
+            Log.i("ActivityLifeCycleStatus", "OnStopped-ObserverEvent");
+            Log.i("ActivityLifeCycleStatus", lifecycleOwner.getLifecycle().getCurrentState().toString()+" State");
         }
         @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
         void ondestroyIsCalled ()
         {
-            Log.i("State", "OnDestroyedState");
+            Log.i("ActivityLifeCycleStatus", "OnDestroyed-ObserverEvent");
+            Log.i("ActivityLifeCycleStatus", lifecycleOwner.getLifecycle().getCurrentState().toString()+" State");
         }
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+    void anEvent(){
+         //Log.i("ActivityLifeCycleStatus", "Any-ObserverEvent");
+           // Log.i("ActivityLifeCycleStatus", lifecycleOwner.getLifecycle().getCurrentState().toString()+" State");
+     }
 
     }
--- LifeCycleAwareComponents/app/src/main/java/com/example/lifecycleawarecomponents/MainActivity.java
+++ LifeCycleAwareComponents/app/src/main/java/com/example/lifecycleawarecomponents/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.lifecycleawarecomponents;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LifecycleOwner;
@@ -12,7 +13,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new BindingClass(this);
+        Log.i("ActivityLifeCycleStatus", "OnCreated-Activity");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("ActivityLifeCycleStatus", "OnStart-Activity");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("ActivityLifeCycleStatus", "OnResumed-Activity");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("ActivityLifeCycleStatus", "OnPaused-Activity");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("ActivityLifeCycleStatus", "OnStopped-Activity");
+    }
 
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("ActivityLifeCycleStatus", "OnRestarted-Activity");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("ActivityLifeCycleStatus", "OnDestroyed-Activity");
     }
 }
